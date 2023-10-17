d9efbd0bfd5e2f4ff88c02551e974b473ce57d81
Adnan Dervisevic <dervisevicado@gmail.com>
2023-10-17 17:50:38 +0200

    LV1

---
+++ LV1/Z1/Z1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Z1 {
+    public static void main(String[] args){
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesite 2 broja:");
+        int a = ulaz.nextInt();
+        int b = ulaz.nextInt();
+        System.out.println("Uneseni brojevi su:");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
