155f57914b904969614ff62d4d91256c53c08896
Adnan Dervisevic <dervisevicado@gmail.com>
2023-10-31 13:19:28 +0100

    prepravljen kod, unos preko args

--- lv2-z1/src/Main.java
+++ lv2-z1/src/Main.java
@@ -2,15 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Unesite broj: ");
 
-        Scanner ulaz = new Scanner(System.in);
-        double br = ulaz.nextDouble();
+        //System.out.print("Unesite broj: ");
 
-        Racun racun = new Racun();
+        //Scanner ulaz = new Scanner(System.in)
+        for(String s:args) {
+            double br = Double.parseDouble(s);
 
-        System.out.println("Faktorijel cijelog dijela unesenog broja: "+racun.faktorijel((int)br));
+            Racun racun = new Racun();
 
-        System.out.println("Sinus: "+racun.sinus(br));
+            System.out.println("Faktorijel cijelog dijela unesenog broja: " + racun.faktorijel((int) br));
+
+            System.out.println("Sinus: " + racun.sinus(br));
+        }
     }
 }
