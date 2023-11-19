f2417d4375c389973203a7d0847b5c61ea57da29
Adnan Dervisevic <dervisevicado@gmail.com>
2023-11-19 12:16:43 +0100

    private atribut; izuzetak u slucaju neispravne ocjene

--- lv5/src/main/java/org/example/Ocjena.java
+++ lv5/src/main/java/org/example/Ocjena.java
@@ -2,15 +2,24 @@ package org.example;
 
 public class Ocjena {
     private LicneInformacije osoba;
-    int ocjena;
+    private int ocjena;
 
     public Ocjena(LicneInformacije osoba, int ocjena){
         this.osoba=osoba;
-        if(ocjena>=0&&ocjena<=10) this.ocjena=ocjena;
-        else this.ocjena=0;
+        setOcjena(ocjena);
+    }
+
+    public LicneInformacije getOsoba() {
+        return osoba;
+    }
+
+    public int getOcjena() {
+        return ocjena;
     }
 
     public void setOcjena(int n){
+
         if(n>=0 && n<=10) ocjena=n;
+        throw new RuntimeException("Neispravna ocjena");
     }
 }
